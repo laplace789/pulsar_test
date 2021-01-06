6ce19e81b3e25331941d7c675f4656f90d4154a6
laplace789 <laplace789@github.com>
2021-01-07 00:02:53 +0800

    add conf dir

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/laplace789/pulsar_test/config"
 	"github.com/laplace789/pulsar_test/input"
 )
 
+var cfgDir = flag.String("conf", "", "config dir")
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
-	cfg := config.Config("./conf/")
+	if *cfgDir == "" {
+		panic("no config file")
+	}
+	cfg := config.Config(*cfgDir)
 	in := input.NewInputer("pulsar")
 	in.Init(cfg)
 	in.PrintStatus()
